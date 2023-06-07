8e2a3691e6458390268d1e2146cd50e907974b4e
RA4Z <rraz639@gmail.com>
2023-06-07 12:17:02 -0300

    Estilizando o cabeçalho do projeto

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -3,18 +3,31 @@ import styled from 'styled-components'
 import bank_logo from "../../assets/images/bank_logo.svg";
 import { corPrimaria } from '../UI/variaveis'
 
+const BtnCabecalho = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  background: ${(props) => (props.primary ? 'white' : corPrimaria)};
+  color: ${(props) => props.primary ? corPrimaria : 'white'};
+  `
+
+
 const StyledHeader = styled.nav`
-    background-color: ${corPrimaria};
-    display: flex;
-    justify-content: space-between;
-    padding: 0 15vw;
-    height: 10vh;
-    align-items: center;
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
 `
 
 const Logo = styled.img`
-    height: 50px;
-    width: 50px;
+  height: 50px;
+  width: 50px;
 `
 
 const Cabecalho = () => {
@@ -22,12 +35,8 @@ const Cabecalho = () => {
     <StyledHeader>
       <Logo src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
-          Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
-          Sair
-        </a>
+        <BtnCabecalho primary href="https://google.com">Ajuda</BtnCabecalho>
+        <BtnCabecalho href="https://google.com">Sair</BtnCabecalho>
       </div>
     </StyledHeader>
   );
